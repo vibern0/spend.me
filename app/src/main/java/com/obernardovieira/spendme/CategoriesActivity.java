4c8e24193c57918d7c6cc514b9ab076ba27c94a3
bernardovieira <iamthekyt@gmail.com>
2016-08-16 14:46:39 +0100

    FinalActivities
    All views created

--- app/src/main/java/com/obernardovieira/spendme/CategoriesActivity.java
+++ app/src/main/java/com/obernardovieira/spendme/CategoriesActivity.java
@@ -1,9 +1,13 @@
 package com.obernardovieira.spendme;
 
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -11,6 +15,25 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+
+        String[] categoryArray = { };
+
+        ListView lv = (ListView) findViewById(R.id.lv_categories);
+        TextView tv = (TextView) findViewById(R.id.tv_no_categories);
+
+        if(categoryArray.length == 0) {
+            lv.setVisibility(View.GONE);
+            tv.setVisibility(View.VISIBLE);
+        }
+        else {
+            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview_categories, categoryArray);
+            lv.setAdapter(adapter);
+
+            lv.setVisibility(View.VISIBLE);
+            tv.setVisibility(View.GONE);
+        }
+
     }
 
     public void goNewCategory(View view) {
