23b05a0b30f2f53996cb9804afd90f888bc16a29
Bernardo Vieira <bernardo5@live.com.pt>
2017-01-31 18:18:12 +0000

    AllLayouts
    All UI elements created!

--- app/src/main/java/myself/obernardovieira/spendme/SpendActivity.java
+++ app/src/main/java/myself/obernardovieira/spendme/SpendActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class SpendActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spend);
     }
